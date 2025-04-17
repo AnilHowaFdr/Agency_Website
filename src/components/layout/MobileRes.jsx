17e6e4e98285d711922da62099571abd9b9891ff
Heron Mia <howa.anil@gmail.com>
2025-04-17 22:59:53 +0600

    navbar

---
+++ src/components/layout/MobileRes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MobileRes = ({ show }) => {
+  return (
+    <div>
+      <div className="bg-red h-screen">MobileRes</div>
+    </div>
+  );
+};
+
+export default MobileRes;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,34 +4,101 @@ import { RiBarChartFill } from "react-icons/ri";
 import { useState } from "react";
 import { VscSearch } from "react-icons/vsc";
 import { BsHandbag } from "react-icons/bs";
+import MobileRes from "./MobileRes";
+
 const Navbar = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+  const [active, setActive] = useState("home");
+  // const navLinks = (
+  //   <ul className=" flex flex-col md:flex-row lg:space-x-8 sm:space-x-4 space-y-2 md:space-y-0 font-medium text-xl p-4 md:p-0">
+  //     <li>
+  //       <Link
+  //         to="/"
+  //         className={`text-[#3661FC] ${active === "home" ? "isActive" : ""} `}
+  //       >
+  //         Home
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link
+  //         to="/about"
+  //         className={`text-[#3661FC] ${active === "about" ? "isActive" : ""} `}
+  //       >
+  //         About
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link
+  //         to="/service"
+  //         className={`text-[#3661FC] ${
+  //           active === "service" ? "isActive" : ""
+  //         } `}
+  //       >
+  //         Service
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link
+  //         to="/team"
+  //         className={`text-[#3661FC] ${active === "team" ? "isActive" : ""} `}
+  //       >
+  //         Team
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link
+  //         to="/blog"
+  //         className={`text-[#3661FC] ${active === "blog" ? "isActive" : ""} `}
+  //       >
+  //         Blog
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link
+  //         to="/news"
+  //         className={`text-[#3661FC] ${active === "news" ? "isActive" : ""} `}
+  //       >
+  //         News
+  //       </Link>
+  //     </li>
+  //     <li>
+  //       <Link
+  //         to="/contact"
+  //         className={`text-[#3661FC] ${
+  //           active === "contact" ? "isActive" : ""
+  //         } `}
+  //       >
+  //         Contact
+  //       </Link>
+  //     </li>
+  //   </ul>
+  // );
+
   return (
-    <nav className=" ">
-      <div className="container">
-        <div className="flex  top-10 left-36 z-10 bg-white rounded-none w-full lg:rounded-full px-5 py-4 flex-col items-center justify-center gap-8 lg:justify-between lg:flex-row">
-          <div className="w-auto lg:w-1/4">
+    <nav className="fixed top-0 right-0 left-0 z-50 bg-white">
+      <div className="container mx-auto px-4 md:px-6 lg:px-8">
+        <div className="flex py-3 lg:py-6 items-center gap-8 md:gap-12 justify-between">
+          <div className="">
             <Link to="/" className="flex items-center gap-2">
-              <img className="w-[66px] h-[66px]" src="/logo.png" alt="image" />
+              <img className="w-10" src="/logo.png" alt="image" />
               <span>
                 <h3 className="uppercase font-bold text-2xl text-primary">
                   Ortency
                 </h3>
-                <p className="capitalize font-medium text-lg text-primary">
+                <p className="capitalize font-medium text-base sm:text-lg text-primary">
                   digital Marketing
                 </p>
               </span>
             </Link>
           </div>
-          <div className="w-full lg:w-1/2 relative">
-            <ul
-              className={`${
-                show ? "-translate-x-0" : "-translate-x-full"
-              } transition-all flex flex-col justify-center items-center gap-8 lg:gap-8 lg:flex-row lg:justify-evenly font-medium text-xl absolute text-white w-full md:w-1/2 md:text-black md:static top-28 left-0  bg-[#3661FC] md:bg-white py-4 md:py-0 h-auto`}
-            >
+          {/* Desktop Responsiveness */}
+          <div className="hidden lg:block">
+            <ul className="transition-all duration-300 flex items-center gap-6 md:gap-3 lg:flex-row font-medium text-base text-white md:text-black">
               <li>
                 <Link
-                  className="flex gap-1 items-center hover:text-[#FE681E]"
+                  className={`flex gap-1 items-center  ${
+                    active === "/" ? "text-[#FE681E]" : "hover:text-[#FE681E]"
+                  }`}
                   to="/"
                 >
                   Home
@@ -42,7 +109,14 @@ const Navbar = () => {
               </li>
 
               <li>
-                <Link className=" hover:text-[#FE681E]" to="/About">
+                <Link
+                  className={`flex gap-1 items-center  ${
+                    active === "/about"
+                      ? "text-[#FE681E]"
+                      : "hover:text-[#FE681E]"
+                  }`}
+                  to="/about"
+                >
                   About
                 </Link>
               </li>
@@ -97,8 +171,8 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <div className="w-full lg:w-1/4">
-            <ul className="flex gap-3 md:gap-8 text-2xl items-center justify-end">
+          <div className="">
+            <ul className="flex gap-3 md:gap-5 text-xl sm:text-2xl items-center justify-end">
               <li>
                 <Link>
                   <VscSearch />
@@ -120,6 +194,8 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
+        {/* Mobile Responsive */}
+        <div></div>
       </div>
     </nav>
   );
